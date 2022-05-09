6d4f52346cce5bc1ee9e776293b4e22f5ec6bb70
usdarys <dariusz.rys2ti@gmail.com>
2022-05-10 00:50:04 +0200

    walka z ogarnieciem frameworka

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+    {{-- <link href="{url}node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{url}css/style.css" rel="stylesheet"> --}}
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title', 'Test Manager')</title>
+</head>
+<body>
+    @yield('body')
+    {{-- <script src="{url}node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script> --}}
+</body>
+</html>
