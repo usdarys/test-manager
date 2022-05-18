163f97091bd4e35beb6ae85d77c9f3d5badd27fd
usdarys <dariusz.rys2ti@gmail.com>
2022-05-19 01:13:08 +0200

    model dla wynikow

---
+++ database/migrations/2022_05_19_005034_create_test_results_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('test_results', function (Blueprint $table) {
+            $table->integer('test_run_id');
+            $table->integer('test_case_id');
+            $table->integer('updated_by');
+            $table->timestamp('updated_at');
+            $table->enum('status', ['untested', 'passed', 'failed']);
+            $table->text('comment')->nullable();
+            $table->foreign('test_run_id')->references('id')->on('test_runs');
+            $table->foreign('test_case_id')->references('id')->on('test_cases');
+            $table->primary(['test_run_id', 'test_case_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('test_results');
+    }
+};
