4f13f0334e7aed68592db5d43a872c6ae2e66b36
usdarys <dariusz.rys2ti@gmail.com>
2022-06-05 21:28:48 +0200

    paginacja + wyszukiwanie ajax - prawie gotowe

--- resources/views/user/page.blade.php
+++ resources/views/user/page.blade.php
@@ -9,46 +9,76 @@
                 <a href="{{ route('user.create') }}" class="btn btn-success">Dodaj</a>
             </li>
             <li>
-                <div class="d-flex ms-3">
-                    <input id="search" class="form-control me-2" type="search" name="search" aria-label="Search" value="">
-                    <button id="searchButton" onclick="handleSearch('{{ url()->current() }}')" class="btn btn-outline-success btn-sm" type="submit">Szukaj</button>                    
+                <div id="search" class="d-flex ms-3">
+                    <input id="searchInput" class="form-control me-2" name="search" aria-label="Search" value="">
+                    <button id="searchButton" class="btn btn-outline-success btn-sm me-2" type="submit">Szukaj</button>    
+                    <button id="clearSearchButton" class="btn btn-outline-danger btn-sm d-none" type="submit">Wyczyść</button>                   
                 </div>
             </li>
         </ul>
-        <div id="pagination-list" onclick="handlePagination(event);">
+        <div id="paginationList">
             @include('user.list')
         </div>
         <script>
-            async function handleSearch(url) {
-                const search = document.getElementById('search').value;
-                if (search) {
-                    url += '?search=' + search;
-                }
-                const resp = await fetch(url, {
-                    headers: {
-                        'X-Requested-With': 'XMLHttpRequest'
+            window.addEventListener('load', (e) => {
+                let search = '';
+                const currentUrl = window.location.href.split('?')[0];
+                const searchInput = document.getElementById('searchInput');
+                const searchButton = document.getElementById('searchButton');
+                const clearSearchButton = document.getElementById('clearSearchButton');
+                const paginationList = document.getElementById('paginationList');
+
+                paginationList.addEventListener('click', (e) => {
+                    let target = event.target;
+                    if (target.className === 'page-link' && target.nodeName === 'A') {
+                        event.preventDefault();
+                        url = target.href;
+                        getData(url);
                     }
                 });
-                if (resp.ok) {
-                    document.getElementById('pagination-list').innerHTML = await resp.text();
+
+                clearSearchButton.addEventListener('click', removeSearch);
+                searchButton.addEventListener('click', applySearch);
+                searchInput.addEventListener('keydown', (e) => {
+                    if (e.key === 'Enter') {
+                        applySearch();
+                    }
+                });
+
+                function applySearch() {
+                    search = searchInput.value;
+                    if (search) {
+                        clearSearchButton.classList.remove('d-none');
+                        getData(currentUrl);
+                    }
                 }
-            }
 
-            async function handlePagination(event) {
-                let target = event.target;
-                let currentTarget = event.currentTarget;
+                function removeSearch() {
+                    search = '';
+                    searchInput.value = '';
+                    clearSearchButton.classList.add('d-none');
+                    getData(currentUrl);
+                }
 
-                if (target.className === 'page-link' && target.nodeName === 'A') {
-                    event.preventDefault();
-                    const resp = await fetch(target.href, {
+                async function getData(url) {
+                    if (search) {
+                        if (url.includes('?')) {
+                            url += '&search=' + search;
+                        } else {
+                            url += '?search=' + search;
+                        }
+                    }
+
+                    const resp = await fetch(url, {
                         headers: {
                             'X-Requested-With': 'XMLHttpRequest'
                         }
                     });
+
                     if (resp.ok) {
-                        currentTarget.innerHTML = await resp.text();
+                        paginationList.innerHTML = await resp.text();
                     }
-                } 
-            }
+                }
+            });
         </script>
 @endsection
